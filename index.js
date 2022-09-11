12c84185e9611c345804f3782054c4a81bf551e9
kekcoke <kevin.oane@yahoo.com>
2022-09-10 20:28:04 -0700

    REST API with leaders and promotions routes

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
-
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -15,6 +16,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+app.use(express.static(__dirname + '/public'));
+
 
 const server = http.createServer(app);
 
